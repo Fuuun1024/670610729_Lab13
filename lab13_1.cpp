085aef74f05dd1ee12b9364674e5c93443c7a1aa
Fuuun1024 <worapopfong@gmail.com>
2024-12-27 17:06:42 +0700

    lab13_1 complete

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,38 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    double sum = 0;
+    double SD = 0;
+    double GM = 1;
+    double HM = 0;
+    double Max = A[0];
+    double Min = A[0];
+    
+    for(int i=0; i<N ;i++){
+        sum += A[i];
+        
+    }
+    double mean = sum/N;
+    
+    for(int i=0; i<N ;i++){
+        SD += pow(A[i]-mean,2);
+        GM *=A[i];
+        HM += 1.0/A[i];
+        if(A[i] >= Max) Max = A[i];
+        if(A[i] <= Min) Min = A[i]; 
+    }
+    
+    
+    double StandardDeviation = pow((SD/N),0.5);
+    double GeometricMean = pow(GM,1.0/N);
+    double HarmonicMean = N/HM;
+    
+    B[0] = mean;
+    B[1] = StandardDeviation;
+    B[2] = GeometricMean;
+    B[3] = HarmonicMean;
+    B[4] = Max;
+    B[5] = Min;
+}
